cbbf83e6c90d96bf1decd177315baa7bc7d07ce3
TaliaPalacio <talia.palacio@udea.edu.co>
2025-05-24 16:27:49 -0500

    refactor: remove unused React import from ConsultPage component

--- app/querys/page.tsx
+++ app/querys/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { FaSearch, FaIdCard, FaRegListAlt } from "react-icons/fa";
 import Link from "next/link";
-import React, { use } from "react";
 
 
 import TransitionPage from "@/components/transition-page";
