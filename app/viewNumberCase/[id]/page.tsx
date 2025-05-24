ada6bf4800cd67826175827c6e22addba3aad10c
Alejandro Becerra Acevedo <Alejandro.becerraa@udea.edu.co>
2025-05-24 16:11:17 -0500

    :sparkles: feat: add ViewNumberCase and ConsultaTabla components for displaying case details and actions

--- app/viewNumberCase/[id]/page.tsx
+++ app/viewNumberCase/[id]/page.tsx
@@ -1,16 +1,17 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
-import { useNumberCase }  from "@/api/hooks/useNumberCase";
+import { useParams, useRouter } from "next/navigation";
+import { useNumberCase } from "@/api/hooks/useNumberCase";
 import { getNumberCaseParams } from "@/api/types/numberCase";
 import { Proceso } from "@/api/types/processType";
 
-
 export default function ViewNumberCase() {
-  
-  const params = useSearchParams();
-  const { data, isLoading, error } = useNumberCase(getNumberCaseParams(params));
+  const params = useParams();
+  const router = useRouter();
+  const numero = params.id as string;
 
+  const searchParams = new URLSearchParams({ numero });
+  const { data, isLoading, error } = useNumberCase(getNumberCaseParams(searchParams));
 
   if (isLoading) return <div className="p-10 text-center text-gray-600">Cargando...</div>;
   if (error) return <div className="p-10 text-red-600">Ocurrió un error al consultar.</div>;
@@ -34,6 +35,13 @@ export default function ViewNumberCase() {
             <p><span className="font-semibold">Sujetos Procesales:</span> {proceso.sujetosProcesales}</p>
             <p><span className="font-semibold">Fecha del Proceso:</span> {new Date(proceso.fechaProceso).toLocaleDateString()}</p>
             <p><span className="font-semibold">Última Actuación:</span> {new Date(proceso.fechaUltimaActuacion).toLocaleDateString()}</p>
+
+            <button
+              onClick={() => router.push(`/viewIAActions/${proceso.idProceso}`)}
+              className="mt-4 bg-[#008BFF] text-white px-4 py-2 text-sm rounded-md font-medium hover:bg-[#0000CD] transition"
+            >
+              Ver Actuaciones
+            </button>
           </div>
         ))}
       </div>
